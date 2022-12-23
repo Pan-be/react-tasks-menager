f084e84a120aefcead23b5eba5cdffde88640667
Konrad <konradbieniek@gmail.com>
2022-12-23 23:49:10 +0100

    little update App.js

--- src/App.js
+++ src/App.js
@@ -4,36 +4,36 @@ import Tasks from "./components/Tasks/Tasks"
 import NewTask from "./components/NewTask/NewTask"
 
 function App() {
-	const [isLoading, setIsLoading] = useState(false)
-	const [error, setError] = useState(null)
-	const [tasks, setTasks] = useState([])
+	// const [isLoading, setIsLoading] = useState(false)
+	// const [error, setError] = useState(null)
+	// const [tasks, setTasks] = useState([])
 
-	const fetchTasks = async (taskText) => {
-		setIsLoading(true)
-		setError(null)
-		try {
-			const response = await fetch(
-				"https://tasks-menager-default-rtdb.firebaseio.com//tasks.json"
-			)
+	// const fetchTasks = async (taskText) => {
+	// 	setIsLoading(true)
+	// 	setError(null)
+	// 	try {
+	// 		const response = await fetch(
+	// 			"https://tasks-menager-default-rtdb.firebaseio.com//tasks.json"
+	// 		)
 
-			if (!response.ok) {
-				throw new Error("Request failed!")
-			}
+	// 		if (!response.ok) {
+	// 			throw new Error("Request failed!")
+	// 		}
 
-			const data = await response.json()
+	// 		const data = await response.json()
 
-			const loadedTasks = []
+	// 		const loadedTasks = []
 
-			for (const taskKey in data) {
-				loadedTasks.push({ id: taskKey, text: data[taskKey].text })
-			}
+	// 		for (const taskKey in data) {
+	// 			loadedTasks.push({ id: taskKey, text: data[taskKey].text })
+	// 		}
 
-			setTasks(loadedTasks)
-		} catch (err) {
-			setError(err.message || "Something went wrong!")
-		}
-		setIsLoading(false)
-	}
+	// 		setTasks(loadedTasks)
+	// 	} catch (err) {
+	// 		setError(err.message || "Something went wrong!")
+	// 	}
+	// 	setIsLoading(false)
+	// }
 
 	useEffect(() => {
 		fetchTasks()
